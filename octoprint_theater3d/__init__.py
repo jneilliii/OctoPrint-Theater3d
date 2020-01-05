3d8af603e48d6eeda097eb5fe6074e31b59e15cd
nerdboyq <princtonb@gmail.com>
2020-01-05 00:39:07 -0800

    attempt2 - commit

---
+++ octoprint_theater3d/__init__.py
@@ -0,0 +1,147 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+### (Don't forget to remove me)
+# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
+# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
+# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
+# as necessary.
+#
+# Take a look at the documentation on what other plugin mixins are available.
+
+import octoprint.plugin
+
+import sys, os
+cd = os.getcwd()
+sys.path.append(cd)
+
+from .libs.servo_ctrlr import PanTilt_Ctrlr
+import octoprint.plugin
+from octoprint.events import eventManager, Events
+from flask import jsonify, request, make_response, Response
+from octoprint.server.util.flask import restricted_access
+from datetime import datetime
+from datetime import timedelta
+import octoprint.util
+import requests
+import inspect
+import threading
+import json
+
+
+class Theater3dPlugin(octoprint.plugin.StartupPlugin,
+                      octoprint.plugin.SettingsPlugin,
+                      octoprint.plugin.AssetPlugin,
+                      octoprint.plugin.TemplatePlugin,
+                      octoprint.plugin.BlueprintPlugin, 
+                      octoprint.plugin.EventHandlerPlugin):
+
+	##~~ Creates an instance of the Pan/Tilt Servo Controller & sets the default values
+	pt_ctrlr = PanTilt_Ctrlr()
+	
+	def on_after_startup(self):
+		self._logger.info("Theater3d is now starting.")
+
+	##~~ SettingsPlugin mixin
+
+	def get_settings_defaults(self):
+		return dict(
+			# put your plugin's default settings here
+		)
+
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		# Define your plugin's asset files to automatically include in the
+		# core UI here.
+		return dict(
+			js=["js/theater3d.js"],
+			css=["css/theater3d.css"],
+			less=["less/theater3d.less"]
+		)
+		
+	@octoprint.plugin.BlueprintPlugin.route("/pan_fxn", methods=["GET"])
+	def pan_fxn(self):
+		direction = request.values["direction"]
+		if (direction == "L"):
+			self.pt_ctrlr.pan_left()
+		elif (direction == "R"):
+			self.pt_ctrlr.pan_right()
+		return jsonify(success=True)
+
+	@octoprint.plugin.BlueprintPlugin.route("/tilt_fxn", methods=["GET"])
+	def tilt_fxn(self):
+		direction = request.values["direction"]
+		if (direction == "U"):
+			self.pt_ctrlr.tilt_up()
+		elif (direction == "D"):
+			self.pt_ctrlr.tilt_down()
+		return jsonify(success=True)	
+
+	@octoprint.plugin.BlueprintPlugin.route("/swap_fxn", methods=["GET"])
+	def swap_fxn(self):
+		swap_category = request.values["opt"]
+		if (opt == "servos"):
+			self.pt_ctrlr.swap_ServoAssignments()
+		elif (opt == "dir_b"):
+			self.pt_ctrlr.swap_pan_rotations()
+			self.pt_ctrlr.swap_tlt_rotations()
+                elif (opt == "dir_p"):
+                        self.pt_ctrlr.swap_pan_rotations()
+                elif (opt == "dir_t"):
+                        self.pt_ctrlr.swap_tlt_rotations()
+                elif (opt == "all"):
+                        self.pt_ctrlr.swap_all()
+		return jsonify(success=True)
+
+	@octoprint.plugin.BlueprintPlugin.route("/reset_fxn", methods=["GET"])
+	def reset_fxn(self):
+		self.pt_ctrlr.reset_Servos()
+		return jsonify(success=True)
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+		# for details.
+		return dict(
+			theater3d=dict(
+				displayName="Theater3d Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="nerdboyq",
+				repo="OctoPrint-Theater3d",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/nerdboyq/OctoPrint-Theater3d/archive/{target_version}.zip"
+			)
+		)
+	
+
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+#__plugin_name__ = "Theater3d Plugin"
+
+# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
+# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
+# compatibility flags according to what Python versions your plugin supports!
+#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
+#__plugin_pythoncompat__ = ">=3,<4" # only python 3
+#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = Theater3dPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
+
---
+++ octoprint_theater3d/libs/servo_ctrlr.py
@@ -0,0 +1,146 @@
+'''
+
+Author : Princton C. Brennan
+Creation Date : December 23, 2019
+
+The Servo Controller module was created with the intention to be used
+in conjuction with octoprint. Due to the low power nature of the Raspberry Pi, 
+servos are a good solution to use for different controls related to the 
+3d printer. This code is compatible w/ python 2.7 through python 3.7, and 
+utilizes the RPi library for Pi GPIO control to send PWM (Pulse Width Modulation)
+signals to the respective servos.  
+
+Be mindful to only wire the servos to PWM capable pins on the Pi; this code was tested
+with the Pi3 B+ which has the following PWM pins: 32 & 33
+The code is written such that the pins must be assigned by the user.
+
+'''
+
+from time import sleep
+import sys, os
+cd = os.getcwd()
+paths=[cd+'/static/python','/usr/lib/python2.7', '/usr/lib/python2.7/plat-arm-linux-gnueabihf', '/usr/lib/python2.7/lib-tk', '/usr/lib/python2.7/lib-old', '/usr/lib/python2.7/lib-dynload', '/home/pi/.local/lib/python2.7/site-packages', '/usr/local/lib/python2.7/dist-packages', '/usr/lib/python2.7/dist-packages']
+for path in paths:
+       sys.path.append(path)
+import RPi.GPIO as GPIO
+
+
+try:
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BOARD)		# This sets the pin references for the PI to the numbered mode
+	GPIO.cleanup()	# Clear any previous settings for the GPIO pins, in case they weren't cleared previously
+except Exception:
+	print("All pins ready for assignment")
+	pass	#if there's nothing to cleanup, the GPIO.cleanup() will be skipped.
+class PanTilt_Ctrlr:
+	pan_pin = 32			# Use this variable to assign the pan servo's signal (pwm) pin
+	tlt_pin = 33			# Use this variable to assign the pan servo's signal (pwm) pin
+	dutyCycle_stop = 0	# Default Duty Cycle value to stop the servo movement is 0
+	delay_time = 0.025	# Default servo turn delay value is 50 milliseconds
+	pan_servo = None	# This variable will be used as a placeholder for the pan servo object
+	tlt_servo = None	# This variable will be used as a placeholder for the tilt servo object
+        pan_ccw_val = 12
+        pan_cw_val = 3
+	tlt_ccw_val = 12
+	tlt_cw_val = 3
+
+	# The method below assigns the servos to their respective positions based on your pin variable definitions
+	def assignServos(self):
+		# If the pan or tilt signal pins are not assigned, the servo's cannot be assigned
+		if (self.pan_pin != 0):
+			try:
+				GPIO.setup(self.pan_pin,GPIO.OUT)  # Sets Pan-Servo signal pin assignment [parameter must be set]
+				self.pan_servo = GPIO.PWM(self.pan_pin, 50)     # Sets Pan-Servo signal pin as a PWM pin
+				self.pan_servo.start(0)               # Starts running PWM on the Pan-Servo signal pin and sets it to 0
+				GPIO.setup(self.tlt_pin,GPIO.OUT)  
+				self.tlt_servo = GPIO.PWM(self.tlt_pin, 50)     
+				self.tlt_servo.start(0) 
+			except Exception:
+				self.stop_ServoSession()
+				GPIO.setmode(GPIO.BOARD)		# This sets the pin references for the PI to the numbered mode
+				GPIO.setup(self.pan_pin,GPIO.OUT)  # Sets Pan-Servo signal pin assignment [parameter must be set]
+				self.pan_servo = GPIO.PWM(self.pan_pin, 50)     # Sets Pan-Servo signal pin as a PWM pin
+				self.pan_servo.start(0)               # Starts running PWM on the Pan-Servo signal pin and sets it to 0
+				GPIO.setup(self.tlt_pin,GPIO.OUT)  
+				self.tlt_servo = GPIO.PWM(self.tlt_pin, 50)     
+				self.tlt_servo.start(0) 
+
+		else:
+			print("Error: Please provide pan & tile servo pins before assigning Servos.")
+
+	# If the commands you provide seem to be controlling the incorrect servo, you can run the method below to swap them
+	def swap_ServoAssignments(self):
+		# The servo swap cannot occur it there are no initial servo assignments 
+		if (self.pan_servo != None):
+			self.pan_servo = GPIO.PWM(self.tlt_pin, 50)     
+			self.pan_servo.start(0)   
+			self.tlt_servo = GPIO.PWM(self.pan_pin, 50)     
+			self.tlt_servo.start(0)               
+		else:
+			print("Error: The servos were never initialized.")
+
+	def swap_pan_rotations(self):
+		if (self.pan_ccw_val == 12):
+			self.pan_ccw_val = 3
+			self.pan_cw_val = 12
+		else:
+                        self.pan_ccw_val = 12
+                        self.pan_cw_val = 3
+
+	def swap_tlt_rotations(self):
+                if (self.tlt_ccw_val == 12):
+                        self.tlt_ccw_val = 3
+                        self.tlt_cw_val = 12
+                else:
+                        self.tlt_ccw_val = 12
+                        self.tlt_cw_val = 3
+
+        def swap_rotations(self):
+		self.swap_pan_rotations()
+		self.swap_tlt_rotations
+
+	def swap_all(self):
+                self.swap_pan_rotations()
+                self.swap_tlt_rotations
+		self.swap_ServoAssignments()
+
+	def pan_left(self):
+		self.pan_servo.ChangeDutyCycle(self.pan_ccw_val)
+		sleep(self.delay_time)
+		self.pan_servo.ChangeDutyCycle(0)
+
+	def pan_right(self):
+		self.pan_servo.ChangeDutyCycle(self.pan_cw_val)
+		sleep(self.delay_time)
+		self.pan_servo.ChangeDutyCycle(0)
+
+	def tilt_down(self):
+		self.tlt_servo.ChangeDutyCycle(self.tlt_ccw_val)
+		sleep(self.delay_time)
+		self.tlt_servo.ChangeDutyCycle(0)
+
+	def tilt_up(self):
+		self.tlt_servo.ChangeDutyCycle(self.tlt_cw_val)
+		sleep(self.delay_time)
+		self.tlt_servo.ChangeDutyCycle(0)
+
+	# Kills the servo control to release the PWM control and pin configuration
+	def stop_ServoSession(self):
+		try:
+			self.pan_servo.ChangeDutyCycle(0)
+			self.tlt_servo.ChangeDutyCycle(0)
+		except Exception:
+			print("no initial servo assignment.")
+		GPIO.cleanup()
+
+	def reset_Servos(self):
+		self.stop_ServoSession()
+		self.AssignServos()
+
+	def __init__(self):
+		self.assignServos()
+	sleep(1)
+
+
+
+
